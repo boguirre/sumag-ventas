a89c8746ad12c2222aa5fcbd54478c6dbb36674d
boguirre <boguirre@gmail.com>
2023-01-18 19:05:45 -0500

    modificacion de la migracion articulo

--- database/migrations/2023_01_18_234439_create_articulos_table.php
+++ database/migrations/2023_01_18_234439_create_articulos_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('articulos', function (Blueprint $table) {
             $table->id();
+            $table->string('codigo');
             $table->string('nombre');
             $table->string('desccripcion');
             $table->integer('stock');

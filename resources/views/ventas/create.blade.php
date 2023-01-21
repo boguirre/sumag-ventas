60f08457a5b34b43f7294d729e987a921eaeeddf
Joan Roy Carlos Vasquez <joanroycar@gmail.com>
2023-01-20 19:45:08 -0500

    Crear Venta

--- resources/views/ventas/create.blade.php
+++ resources/views/ventas/create.blade.php
@@ -52,8 +52,8 @@
                 </div>
 
                     <div class="col-md-3">
-                        <label for="quantity">Cantidad</label>
-                        <input type="number" class="form-control" name="quantity" id="quantity" aria-describedby="helpId">
+                        <label for="cantidad">Cantidad</label>
+                        <input type="number" class="form-control" name="cantidad" id="cantidad" aria-describedby="helpId">
                     </div>
                     <div class="form-group col-md-3">
                         <label for="tax">Impuesto</label>
@@ -61,7 +61,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="basic-addon3">%</span>
                             </div>
-                            <input type="number" class="form-control" name="tax" id="tax" aria-describedby="basic-addon3" value="18">
+                            <input type="number" class="form-control" name="impuesto" id="impuesto" aria-describedby="basic-addon3" value="18">
                         </div>
                     </div>
                     <div class="form-group col-md-3">
@@ -70,7 +70,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="basic-addon2">%</span>
                             </div>
-                            <input type="number" class="form-control" name="discount" id="discount" aria-describedby="basic-addon2" value="0">
+                            <input type="number" class="form-control" name="descuento" id="descuento" aria-describedby="basic-addon2" value="0">
                         </div>
                     </div>
                     </div>
@@ -127,7 +127,12 @@
                         </div>
                     </div>
                     
-                    
+                    <div class="card-footer text-muted">
+                        <button type="submit" id="guardar" class="btn btn-primary float-right">Registrar</button>
+                         <a href="{{route('venta.index')}}" class="btn btn-light">
+                            Cancelar
+                         </a>
+                    </div>
                     
                     {!! Form::close() !!}
                 </div>
@@ -161,16 +166,16 @@ function agregar() {
 
     product_id = datosProducto[0];
     producto = $("#articulo_id option:selected").text();
-    quantity = $("#quantity").val();
-    discount = $("#discount").val();
+    quantity = $("#cantidad").val();
+    discount = $("#descuento").val();
     price = $("#precio_venta").val();
     stock = $("#stock").val();
-    impuesto = $("#tax").val();
+    impuesto = $("#impuesto").val();
     if (product_id != "" && quantity != "" && quantity > 0 && discount != "" && price != "") {
         if (parseInt(stock) >= parseInt(quantity)) {
             subtotal[cont] = (quantity * price) - (quantity * price * discount / 100);
             total = total + subtotal[cont];
-            var fila = '<tr class="selected" id="fila' + cont + '"><td><button type="button" class="btn btn-danger btn-sm" onclick="eliminar(' + cont + ');">                            <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                            <path fill-rule="evenodd" clip-rule="evenodd" d="M20.2871 5.24297C20.6761 5.24297 21 5.56596 21 5.97696V6.35696C21 6.75795 20.6761 7.09095 20.2871 7.09095H3.71385C3.32386 7.09095 3 6.75795 3 6.35696V5.97696C3 5.56596 3.32386 5.24297 3.71385 5.24297H6.62957C7.22185 5.24297 7.7373 4.82197 7.87054 4.22798L8.02323 3.54598C8.26054 2.61699 9.0415 2 9.93527 2H14.0647C14.9488 2 15.7385 2.61699 15.967 3.49699L16.1304 4.22698C16.2627 4.82197 16.7781 5.24297 17.3714 5.24297H20.2871ZM18.8058 19.134C19.1102 16.2971 19.6432 9.55712 19.6432 9.48913C19.6626 9.28313 19.5955 9.08813 19.4623 8.93113C19.3193 8.78413 19.1384 8.69713 18.9391 8.69713H5.06852C4.86818 8.69713 4.67756 8.78413 4.54529 8.93113C4.41108 9.08813 4.34494 9.28313 4.35467 9.48913C4.35646 9.50162 4.37558 9.73903 4.40755 10.1359C4.54958 11.8992 4.94517 16.8102 5.20079 19.134C5.38168 20.846 6.50498 21.922 8.13206 21.961C9.38763 21.99 10.6811 22 12.0038 22C13.2496 22 14.5149 21.99 15.8094 21.961C17.4929 21.932 18.6152 20.875 18.8058 19.134Z" fill="currentColor"></path>                            </svg>                        </button></td> <td><input type="hidden" name="product_id[]" value="' + product_id + '">' + producto + '</td> <td> <input type="hidden" name="price[]" value="' + parseFloat(price).toFixed(2) + '"> <input class="form-control" type="number" value="' + parseFloat(price).toFixed(2) + '" disabled> </td> <td> <input type="hidden" name="discount[]" value="' + parseFloat(discount) + '"> <input class="form-control" type="number" value="' + parseFloat(discount) + '" disabled> </td> <td> <input type="hidden" name="quantity[]" value="' + quantity + '"> <input type="number" value="' + quantity + '" class="form-control" disabled> </td> <td align="right">s/' + parseFloat(subtotal[cont]).toFixed(2) + '</td></tr>';
+            var fila = '<tr class="selected" id="fila' + cont + '"><td><button type="button" class="btn btn-danger btn-sm" onclick="eliminar(' + cont + ');">                            <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                            <path fill-rule="evenodd" clip-rule="evenodd" d="M20.2871 5.24297C20.6761 5.24297 21 5.56596 21 5.97696V6.35696C21 6.75795 20.6761 7.09095 20.2871 7.09095H3.71385C3.32386 7.09095 3 6.75795 3 6.35696V5.97696C3 5.56596 3.32386 5.24297 3.71385 5.24297H6.62957C7.22185 5.24297 7.7373 4.82197 7.87054 4.22798L8.02323 3.54598C8.26054 2.61699 9.0415 2 9.93527 2H14.0647C14.9488 2 15.7385 2.61699 15.967 3.49699L16.1304 4.22698C16.2627 4.82197 16.7781 5.24297 17.3714 5.24297H20.2871ZM18.8058 19.134C19.1102 16.2971 19.6432 9.55712 19.6432 9.48913C19.6626 9.28313 19.5955 9.08813 19.4623 8.93113C19.3193 8.78413 19.1384 8.69713 18.9391 8.69713H5.06852C4.86818 8.69713 4.67756 8.78413 4.54529 8.93113C4.41108 9.08813 4.34494 9.28313 4.35467 9.48913C4.35646 9.50162 4.37558 9.73903 4.40755 10.1359C4.54958 11.8992 4.94517 16.8102 5.20079 19.134C5.38168 20.846 6.50498 21.922 8.13206 21.961C9.38763 21.99 10.6811 22 12.0038 22C13.2496 22 14.5149 21.99 15.8094 21.961C17.4929 21.932 18.6152 20.875 18.8058 19.134Z" fill="currentColor"></path>                            </svg>                        </button></td> <td><input type="hidden" name="articulo_id[]" value="' + product_id + '">' + producto + '</td> <td> <input type="hidden" name="precio[]" value="' + parseFloat(price).toFixed(2) + '"> <input class="form-control" type="number" value="' + parseFloat(price).toFixed(2) + '" disabled> </td> <td> <input type="hidden" name="descuento[]" value="' + parseFloat(discount) + '"> <input class="form-control" type="number" value="' + parseFloat(discount) + '" disabled> </td> <td> <input type="hidden" name="cantidad[]" value="' + quantity + '"> <input type="number" value="' + quantity + '" class="form-control" disabled> </td> <td align="right">s/' + parseFloat(subtotal[cont]).toFixed(2) + '</td></tr>';
             cont++;
             limpiar();
             totales();

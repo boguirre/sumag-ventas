c6764c2eaabf08230f25fb2b11c4c4e9cd16ae71
boguirre <boguirre@gmail.com>
2023-01-24 19:58:28 -0500

    detalles prestamo controller

---
+++ app/Http/Controllers/PrestamoDetalleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PrestamoDetalle;
+use Illuminate\Http\Request;
+
+class PrestamoDetalleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PrestamoDetalle  $prestamoDetalle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PrestamoDetalle $prestamoDetalle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PrestamoDetalle  $prestamoDetalle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PrestamoDetalle $prestamoDetalle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PrestamoDetalle  $prestamoDetalle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PrestamoDetalle $prestamoDetalle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PrestamoDetalle  $prestamoDetalle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PrestamoDetalle $prestamoDetalle)
+    {
+        //
+    }
+}

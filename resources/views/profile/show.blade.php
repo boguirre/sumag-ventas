10d881217fe67721a9ef63fef85a91b3e78ff59e
Joan Roy Carlos Vasquez <joanroycar@gmail.com>
2023-02-17 16:24:52 -0500

    Habilitado cambiar contraseñas

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -13,13 +13,13 @@
                 <x-jet-section-border />
             @endif
 
-            {{-- @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.update-password-form')
                 </div>
 
                 <x-jet-section-border />
-            @endif --}}
+            @endif
 
             {{-- @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
                 <div class="mt-10 sm:mt-0">
